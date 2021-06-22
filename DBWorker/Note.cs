795b67bd4b2552a3d82426b62c4f6f0a8bd40243
Артём Кармальков <artemkarmalkov@gmail.com>
2021-06-22 21:57:54 +0300

    commit

---
+++ DBWorker/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBWorker
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public double FirstNum { get; set; }
+        public double SecondNum { get; set; }
+        public double Result { get; set; }
+        public int Operation { get; set; }
+    }
+}
---
+++ DBWorker/NoteContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DBWorker
+{
+    class NoteContext : DbContext
+    {
+        public NoteContext() : base("DbConnection") { }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
